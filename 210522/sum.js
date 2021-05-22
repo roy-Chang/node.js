387a002dedb463adb584102ad86e265a2d7074ce
roy-Chang <aul941074@gmail.com>
2021-05-22 16:23:06 +0800

    update sum.js

--- 210522/sum.js
+++ 210522/sum.js
@@ -1,10 +1,10 @@
 function sum(n) {
     let result = 0;
-    for (let i = 1; i <= n; i++) {
+    for (let i = n; i >= 0; i--) {
         result = result + i;
     }
     return result;
 }
 
 
-console.log(sum(3));
+console.log(sum(10));
