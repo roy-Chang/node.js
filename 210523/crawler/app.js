04d9738a4eaccb178a098dece0c478eda524e3bb
roy-Chang <aul941074@gmail.com>
2021-05-29 12:07:40 +0800

    update app.js

---
+++ 210523/crawler/app.js
@@ -0,0 +1,14 @@
+const axios = require('axios');
+
+axios.get('https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=20210523&stockNo=2610')
+    .then(function (response) {
+        // handle success
+        console.log(response);
+    })
+    .catch(function (error) {
+        // handle error
+        console.log(error);
+    })
+    .then(function () {
+        // always executed
+    });
