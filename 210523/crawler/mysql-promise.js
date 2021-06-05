1a8edd001a1223d79659f63ca2fc52252073045c
roy-Chang <aul941074@gmail.com>
2021-06-06 03:14:42 +0800

    update mysql-promise.js  更新功能更符合題目

--- 210523/crawler/mysql-promise.js
+++ 210523/crawler/mysql-promise.js
@@ -17,38 +17,31 @@ connection = Promise.promisifyAll(connection);
 
 (async function () {
     try {
-        let searchResult = await fs.readFile("stock.txt", "utf8")
-            .then((stockCode) => {
-                return axios.get(`http://www.twse.com.tw/zh/api/codeQuery?query=${stockCode}`)
-            })
-            .then((response) => {
-                let answer = response.data.suggestions.shift();
-                let answers = answer.split("\t");
-                if (answers.length > 1) {
-                    return answers;
+        let stockCode = await fs.readFile("stock.txt", "utf8");
+        await connection.connectAsync();
+        let result = await connection.queryAsync("SELECT * FROM stock");
+        if (result.length >= 0) {
+            let dbSearch = await connection.queryAsync("SELECT * FROM stock WHERE stock_id = ?", [stockCode]);
+            if (dbSearch.length == 0) {
+                let response = await axios.get(`http://www.twse.com.tw/zh/api/codeQuery?query=${stockCode}`);
+                let answer = await response.data.suggestions.shift().split("\t");
+                if (answer.length > 1) {
+                    await connection.queryAsync("INSERT INTO stock (stock_id, stock_name) VALUES (?, ?)", [answer[0], answer[1]])
+                    console.log("更新資料庫");
                 } else {
                     console.log(answer[1]);
                     throw "查不到名稱"
                 }
-            })
-            .catch((err) => {
-                console.error(err);
-            })
-        await connection.connectAsync();
-        let result = await connection.queryAsync("SELECT * FROM stock");
-        if (result.length >= 0) {
-            let dbSearch = await connection.queryAsync("SELECT * FROM stock WHERE stock_id = ?", [searchResult[0]]);
-            if (dbSearch.length == 0) {
-                await connection.queryAsync("INSERT INTO stock (stock_id, stock_name) VALUES (?, ?)", [searchResult[0], searchResult[1]])
-                console.log("更新資料庫");
+            } else {
+                console.log("資料存在, ", dbSearch);
             }
         } else {
             console.log("資料庫沒有資料");
         }
-
     } catch (err) {
         console.error(err);
     } finally {
         connection.end();
     }
 })();
+
