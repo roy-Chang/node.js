060741af9bf977f459a41b734389055f1e168933
roy-Chang <aul941074@gmail.com>
2021-06-06 09:21:32 +0800

    update mysql-promise 刪除多餘的判斷式

--- 210523/crawler/mysql-promise.js
+++ 210523/crawler/mysql-promise.js
@@ -19,28 +19,23 @@ connection = Promise.promisifyAll(connection);
     try {
         let stockCode = await fs.readFile("stock.txt", "utf8");
         await connection.connectAsync();
-        let result = await connection.queryAsync("SELECT * FROM stock");
-        if (result.length >= 0) {
-            let dbSearch = await connection.queryAsync("SELECT * FROM stock WHERE stock_id = ?", [stockCode]);
-            if (dbSearch.length == 0) {
-                let response = await axios.get(`http://www.twse.com.tw/zh/api/codeQuery?query=${stockCode}`);
-                if (response.data.suggestions != "(無符合之代碼或名稱)") {
-                    let answer = await response.data.suggestions.shift().split("\t");
-                    if (answer.length >= 1) {
-                        await connection.queryAsync("INSERT INTO stock (stock_id, stock_name) VALUES (?, ?)", [answer[0], answer[1]])
-                        console.log("更新資料庫");
-                    } else {
-                        console.log(answer[1]);
-                        throw "查不到名稱"
-                    }
+        let dbSearch = await connection.queryAsync("SELECT * FROM stock WHERE stock_id = ?", [stockCode]);
+        if (dbSearch.length == 0) {
+            let response = await axios.get(`http://www.twse.com.tw/zh/api/codeQuery?query=${stockCode}`);
+            if (response.data.suggestions != "(無符合之代碼或名稱)") {
+                let answer = await response.data.suggestions.shift().split("\t");
+                if (answer.length >= 1) {
+                    await connection.queryAsync("INSERT INTO stock (stock_id, stock_name) VALUES (?, ?)", [answer[0], answer[1]])
+                    console.log("更新資料庫");
                 } else {
-                    console.log("無此股票，請確認股票代碼是否正確");
+                    console.log(answer[1]);
+                    throw "查不到名稱"
                 }
             } else {
-                console.log("資料存在, ", dbSearch);
+                console.log("無此股票，請確認股票代碼是否正確");
             }
         } else {
-            console.log("資料庫沒有資料");
+            console.log("資料存在, ", dbSearch);
         }
     } catch (err) {
         console.error(err);
