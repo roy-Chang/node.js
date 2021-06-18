b16d5336c72e922e7a2096e35bdc7b0587fe6dd1
roy-Chang <aul941074@gmail.com>
2021-06-18 16:07:16 +0800

    新增pug 語法網頁

--- 210523/simple-web/express.js
+++ 210523/simple-web/express.js
@@ -11,6 +11,12 @@ let app = express();
 // 自動幫你為 public 裡面的檔案建立路由
 app.use(express.static("public"));
 
+// 第一個 : views 變數
+// 第二個 : views 檔案夾名稱
+app.set("views", "views");
+// 告訴 express 我們用的 view engine 是 pug
+app.set("view engine", "pug");
+
 
 app.use(function (req, res, next) {
     let current = new Date();
@@ -22,10 +28,12 @@ app.use(function (req, res, next) {
 
 // 路由 router
 app.get("/", function (req, res) {
-    res.send("Hello Express")
+    // res.send("Hello Express")
+    res.render("index");
 })
 app.get("/about", function (req, res) {
-    res.send("About Express")
+    // res.send("About Express")
+    res.render("about");
 })
 app.get("/test", function (req, res) {
     res.send("Test Express")
