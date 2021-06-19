36588731f323897d1e3d61b706a7f7488aa0f17f
roy-Chang <aul941074@gmail.com>
2021-06-19 16:16:49 +0800

    新增fetch

--- 210523/simple-web/public/javascripts/api.js
+++ 210523/simple-web/public/javascripts/api.js
@@ -1,34 +1,20 @@
-// $(function () {
-//     new Vue({
-//         el: "#app",
-//         data: {
-//             stock: [],
-//         },
-//         beforeMount: async function () {
-//             let response = await fetch("api/stock");
-//             this.stocks = await response.json();
-//         },
-//     })
-// })
-
-
-// $.ajax({
-//     method: "GET",
-//     url: '/api/stock',
-//     dataType: "json",
-// })
-//     .done(function (data) {
-//         console.log(data);
-//     }).fail(function (error) {
-//         console.log(error);
-//     })
-//     .always(function () {
-//         console.log("complete");
-//     });
+// a.jax
+$.ajax({
+    method: "GET",
+    url: '/api/stock',
+    dataType: "json",
+})
+    .done(function (data) {
+        console.log(data);
+    }).fail(function (error) {
+        console.log(error);
+    })
+    .always(function () {
+        console.log("complete");
+    });
 
 
 // axios
-
 axios.get('/api/stock')
     .then(function (response) {
         // handle success
@@ -41,3 +27,29 @@ axios.get('/api/stock')
     .then(function () {
         // always executed
     });
+
+
+// fetch
+fetch('/api/stock')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+    });
+
+
+
+// Vue?    
+// $(function () {
+//     new Vue({
+//         el: "#app",
+//         data: {
+//             stock: [],
+//         },
+//         beforeMount: async function () {
+//             let response = await fetch("api/stock");
+//             this.stocks = await response.json();
+//         },
+//     })
+// })
