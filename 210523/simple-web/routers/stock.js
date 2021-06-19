e536dc31df7163939d2669b141d52280688c211e
roy-Chang <aul941074@gmail.com>
2021-06-19 12:55:44 +0800

    新增資料分頁功能

--- 210523/simple-web/routers/stock.js
+++ 210523/simple-web/routers/stock.js
@@ -12,11 +12,41 @@ router.get("/", async (req, res) => {
 })
 
 router.get("/:stockCode", async (req, res) => {
+
     let queryResultsStocks = await connection.queryAsync("SELECT * FROM stock WHERE stock_id = ?;", req.params.stockCode);
-    let queryResultsPrices = await connection.queryAsync("SELECT*FROM stock_price WHERE stock_id = ? ORDER BY date;", req.params.stockCode)
+
+
+
+    let count = await connection.queryAsync("SELECT COUNT(*) as total FROM stock_price WHERE stock_id = ?;", req.params.stockCode);
+
+    const total = count[0].total; //總共幾筆
+    console.log(total);
+    const perPage = 10; // 一頁10筆
+    const lastPage = Math.ceil(total / perPage) + 1; // 總共幾頁
+
+    const currentPage = req.query.page || 1; // 新增page頁 預設第一頁
+    const offset = (currentPage - 1) * perPage;
+
+
+    if (queryResultsStocks.length === 0) {
+        throw new Error("查無代碼");
+    }
+
+    queryResultsStocks = queryResultsStocks[0]; //??
+
+    let queryResultsPrices = await connection.queryAsync(
+        "SELECT*FROM stock_price WHERE stock_id = ? ORDER BY date LIMIT ? OFFSET ?;"
+        , [req.params.stockCode, perPage, offset]
+    );
+
     res.render("stock/detail", {
         stocks: queryResultsStocks,
-        stockPrices: queryResultsPrices
+        stockPrices: queryResultsPrices,
+        pagination: {
+            lastPage,
+            currentPage,
+            total,
+        }
     });
 })
 
