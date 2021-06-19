255bf70e0e70bb18f1392fabd21e7f1d451cd26b
roy-Chang <aul941074@gmail.com>
2021-06-19 10:15:22 +0800

    新增dateStrings

--- 210523/simple-web/utils/db.js
+++ 210523/simple-web/utils/db.js
@@ -8,7 +8,8 @@ let connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB_DATABASE
+    database: process.env.DB_DATABASE,
+    dateStrings: true
 });
 connection = Promise.promisifyAll(connection);
 
